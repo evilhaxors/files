db3d45bc0f4c74dc904c06c7d71ee65093fa36e5
BHUTUU <raj259942@gmail.com>
2021-10-08 14:25:18 +0530

    done successfully

--- server.sh
+++ server.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/bin/bash6
+printf "\033[32mremember username:\033[34m evilhaxors\033[00m\n"
+printf "\n\033[32mremember password:\033[36m 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\033[00m\n\n"
 git add .
 git commit -m "done successfully"
+git push
 git config credential.helper store
-git push --username "evilhaxors" --password "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"
