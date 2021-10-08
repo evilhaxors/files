e3eb0c896141a83f0435bfa73991ab0a2156460e
BHUTUU <raj259942@gmail.com>
2021-10-08 14:26:37 +0530

    done successfully

--- server.sh
+++ server.sh
@@ -1,4 +1,4 @@
-#!/bin/bash6
+#!/bin/bashh
 printf "\033[32mremember username:\033[34m evilhaxors\033[00m\n"
 printf "\n\033[32mremember password:\033[36m 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\033[00m\n\n"
 git add .
