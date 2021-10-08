96e9079d6b1cb8bdecb15f022cc2e1e8ef86faca
BHUTUU <raj259942@gmail.com>
2021-10-08 15:55:50 +0530

    done successfully

--- server.sh
+++ server.sh
@@ -1,6 +1,8 @@
 #!/bin/bashh
 printf "\033[32mremember username:\033[34m evilhaxors\033[00m\n"
-printf "\n\033[32mremember password:\033[36m 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\033[00m\n\n"
+printf "\n\033[32mremember password:\033[34m"
+printf "Z2hwX0lBRlBuc1ZWeVk3VlpxdDZ0M2pMRElSdHpmd1dpSzBBYTFETQo=" | base64 --decode
+echo
 git add .
 git commit -m "done successfully"
 git push
