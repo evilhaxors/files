d0cda1b3242288285ef53cd244524d5d82f974cf
BHUTUU <raj259942@gmail.com>
2021-10-09 11:51:29 +0530

    lol

--- server.sh
+++ server.sh
@@ -3,7 +3,8 @@ printf "\033[32mremember username:\033[34m evilhaxors\033[00m\n"
 printf "\n\033[32mremember password:\033[34m"
 printf "Z2hwX0lBRlBuc1ZWeVk3VlpxdDZ0M2pMRElSdHpmd1dpSzBBYTFETQo=" | base64 --decode
 echo
+read -p $"enter any thing here: " com
 git add .
-git commit -m "done successfully"
+git commit -m "$com"
 git push
 #git config credential.helper store
